0aeda2801871725ef7b05a4bf844ba0f5d5c8f06
Prateek <106758759+Prateek7071@users.noreply.github.com>
2025-12-31 20:06:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let count  = 0;
+const button  = document.querySelector("#incrementBtn")
+const counter = document.querySelector("#counter")
+
+button.addEventListener('click',()=>{
+	alert(count)
+	count ++;
+	p.textContent = count
+})
